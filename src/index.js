e1558c35732d9f739e613586443a310925fd68a5
Keith Butler <20089137@mail.wit.ie>
2022-10-23 22:47:32 +0100

    Added Upcoming movies page.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import {createRoot} from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
 import {Link} from 'react-router-dom'
@@ -14,6 +15,7 @@ const App = () => {
       <SiteHeader />      {/* New Header  */}
       <Routes>
         <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
+        <Route exact path="/movies/upcoming" element={<UpcomingMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
         <Route path="/" element={<HomePage />} />
