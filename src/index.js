7417102a721705b4d56e5c349b3470f56eed37ae
Keith Butler <20089137@mail.wit.ie>
2022-10-23 22:29:59 +0100

    Full review page.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import {createRoot} from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-import favouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
+import MovieReviewPage from "./pages/movieReviewPage";
 import {Link} from 'react-router-dom'
 
 const App = () => {
@@ -18,8 +19,9 @@ const App = () => {
         </li>
       </ul>
       <Routes>
-        <Route exact path="/movies/favourites" element={<favouriteMoviesPage />} />
+        <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
       </Routes>
